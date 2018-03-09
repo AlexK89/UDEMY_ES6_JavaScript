eb0efa00c839c18a5587493e6ff8c2df22bbb6c1
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-09 20:23:51 +0000

    filter array helper method

--- js/filter_helper.js
+++ js/filter_helper.js
@@ -1,7 +1,18 @@
-const products = [{ name: 'banana', price: 2.4 }, { name: 'apple', price: 1.3 }, { name: 'orange', price: 2 }, { name: 'plume', price: 3.1 }, { name: 'raspberry', price: 2.7 }];
+const products = [{ name: 'banana', type: 'fruit', price: 2.4, quantity: 25 }, { name: 'apple', type: 'fruit', price: 1.3, quantity: 12 }, { name: 'carrot', type: 'vegetable', price: 2, quantity: 3 }, { name: 'plume', type: 'fruit', price: 3.1, quantity: 0 }, { name: 'raspberry', type: 'fruit', price: 2.7, quantity: 20 }];
 
+//Filter by price
 const priceUnderTwoPounds = products.filter(product => {
 	return product.price >= 2.4;
 });
 
 console.log(priceUnderTwoPounds);
+console.log('===========================');
+
+//filter by availability and type
+
+const availableFruits = products.filter(product => {
+	return product.type === 'fruit' && product.quantity > 0;
+});
+
+console.log(availableFruits);
+console.log('===========================');
