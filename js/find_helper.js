057af54d4fa8e5508db186f1bfba46d3164ce24b
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-09 20:58:28 +0000

    find array helper method

---
+++ js/find_helper.js
@@ -0,0 +1,9 @@
+//Find element
+
+const products = [{ name: 'banana', type: 'fruit', price: 2.4, quantity: 25 }, { name: 'apple', type: 'fruit', price: 1.3, quantity: 12 }, { name: 'carrot', type: 'vegetable', price: 2, quantity: 3 }, { name: 'plume', type: 'fruit', price: 3.1, quantity: 0 }, { name: 'raspberry', type: 'fruit', price: 2.7, quantity: 20 }];
+
+const carrot = products.find(product => {
+	return product.name === 'carrot';
+});
+console.log(carrot);
+console.log('===========================');
