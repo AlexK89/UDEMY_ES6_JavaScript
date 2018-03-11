c6c2896429181494769fa715b19405a7f65aeb1c
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-11 13:11:48 +0000

    reduce array helper method

--- js/ES6/reduce_helper.js
+++ js/ES6/reduce_helper.js
@@ -1,9 +1,13 @@
 //Sum elements in array
 
 const numbers = [10, 20, 30];
-let sum = numbers.reduce((number, sum) => {
-	return sum + number
-}, 0);
+let sum = numbers.reduce((number, sum) => sum + number);
 
-console.log(sum);
+//OR
+let sumNum = numbers.reduce((number, sum) => {
+	return sum + number
+}, 0); //0 - is a starting point/initial value
+
+console.log('Sum: ', sum);
+console.log('SumNum: ', sumNum);
 console.log('======================');
