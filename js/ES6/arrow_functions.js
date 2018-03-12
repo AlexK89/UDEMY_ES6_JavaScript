42216791f5432210bff135a012ec336c4c609fc9
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-12 23:09:35 +0000

    arrow functions

--- js/ES6/arrow_functions.js
+++ js/ES6/arrow_functions.js
@@ -9,3 +9,14 @@ const doubledNumbers = numbers.map(number => number * 2);
 
 console.log(doubledNumbers);
 
+//Team
+
+const team = {
+	members: ['Jill', 'Bill', 'Alex'],
+	teamName: 'Adventures',
+	teamSummary: function() {
+		return this.members.map(member => `${member} is in ${this.teamName}`); // '=>' here is lexical this
+	}
+};
+
+console.log(team.teamSummary());
