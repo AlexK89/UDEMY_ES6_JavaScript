54f5549d5dff6c018cfc7c9d1ce862d3b1674b79
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-12 22:42:31 +0000

    arrow functions

--- js/ES6/arrow_functions.js
+++ js/ES6/arrow_functions.js
@@ -1,3 +1,11 @@
 const add = (a, b) => a + b;
 
 console.log(add(2,3));
+
+//double array
+
+const numbers = [1,2,3,4];
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
+
