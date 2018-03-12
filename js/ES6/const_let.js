921e8bee7eddbe15bc973ffbfe83dc8cc3d029ca
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-12 20:57:03 +0000

    const and let

---
+++ js/ES6/const_let.js
@@ -0,0 +1,15 @@
+//ES5
+var name = 'Alex';
+var role = 'Software Engineer';
+var hourlyRate = 40;
+
+//ES6
+
+const name = 'Alex'; //as name we do not expect to change
+let role = 'Software Engineer'; //we may change in future
+let hourlyRate = 40; //we may change in future
+
+//some time later
+
+role = 'Senior Software Engineer';
+hourlyRate = 45;
