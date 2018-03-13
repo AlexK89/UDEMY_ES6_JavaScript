a11272585a5de6ac5a72863003cbb901950b60a4
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-13 19:21:19 +0000

    object litterals

--- js/ES6/object_literals.js
+++ js/ES6/object_literals.js
@@ -1,4 +1,6 @@
-function createBookShop(inventory) {
+// Old fashion way
+
+function createBookShopOld(inventory) {
 	return {
 		inventory: inventory,
 		inventoryValue: function () {
@@ -16,7 +18,34 @@ const inventory = [
 	{title: 'Modern VueJS', price: 15}
 ];
 
-const bookShop = createBookShop(inventory);
+const bookShop = createBookShopOld(inventory);
 
 console.log('Inventory value is: ',bookShop.inventoryValue());
 console.log('Price for PHP book is: ', bookShop.priceForTitle('PHP'));
+console.log('=====================');
+
+
+// New fashion way
+
+function createBookShopNew(inventory) {
+	return {
+		inventory,
+		inventoryValue() {
+			return this.inventory.reduce((sum, book) => sum + book.price, 0);
+		},
+		priceForTitle(title) {
+			return this.inventory.find(book => book.title === title).price //return price by title
+		}
+	}
+}
+
+const inventoryNew = [
+	{title: 'Javascript', price: 10},
+	{title: 'PHP', price: 8},
+	{title: 'Modern VueJS', price: 15}
+];
+
+const bookShopNew = createBookShopNew(inventoryNew);
+
+console.log('Inventory value is: ',bookShopNew.inventoryValue());
+console.log('Price for PHP book is: ', bookShopNew.priceForTitle('Javascript'));
