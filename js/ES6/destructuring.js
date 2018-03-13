fbb07b7875c5018931163ee468a9eeeab5517ec4
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-13 21:19:48 +0000

    destructuring in ES6

--- js/ES6/destructuring.js
+++ js/ES6/destructuring.js
@@ -30,3 +30,27 @@ function fileSummary({extension, name, size}) {
 }
 
 console.log(fileSummary(savedFile));
+
+//Array destructuring
+
+const companies = [
+	'Google',
+	'Facebook',
+	'Apple',
+	'Microsoft'
+];
+
+const [ name ] = companies; //Google, the first item.
+const [ name1, name2, name3, ] = companies; //We can get more names;
+console.log(name1, name2, name3);
+
+const [ name5, name6, , name7 ] = companies; //We can skip names;
+console.log(name5, name6, name7);
+
+const [ name10, ...rest ] = companies;
+console.log(rest); //return all except first one;
+
+const {length} = companies;
+console.log(length); //array length
+
+
