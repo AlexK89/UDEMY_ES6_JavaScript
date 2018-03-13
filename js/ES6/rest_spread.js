ab19cad8c0318d863a47dcd7606d65e3652aad01
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-13 19:59:09 +0000

    rest and spread operators

--- js/ES6/rest_spread.js
+++ js/ES6/rest_spread.js
@@ -1,5 +1,14 @@
 function addNumbers(numbers = [0]) {
-	return numbers.reduce((sum, number) => {
-		return sum + number;
-	}, 0)
+	return numbers.reduce((sum, number) => sum + number, 0)
 }
+
+console.log(addNumbers([1,2,3,4,5]));
+
+//REST
+function addNumbersNew(...numbers) { //...numbers put all your arguments into the array
+	return numbers.reduce((sum, number) => sum + number, 0)
+}
+
+console.log(addNumbersNew(1,2,3,4,5)); // list of arguments
+
+//Spread
