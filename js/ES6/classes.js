2cc86aaafa664b880eebe084888e11e8183b9b0a
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-14 19:39:40 +0000

    classes  in ES6

--- js/ES6/classes.js
+++ js/ES6/classes.js
@@ -65,3 +65,22 @@ console.log('===============');
 console.log(opel.honk());
 console.log(opel.color);
 console.log(opel.drive());
+
+//Exercise
+class Monster {
+	constructor(options) {
+		this.health = 100;
+		this.name = options.name;
+	}
+}
+
+class Snake extends Monster {
+	bite(snake) {
+		return snake.health -= 10;
+	}
+}
+
+const snake1 = new Snake({name: 'Nagini'});
+const snake2 = new Snake({name: 'Snek'});
+
+snake1.bite(snake2);
