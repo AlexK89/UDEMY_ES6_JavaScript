af6633d48654a0328cd40f7c5fb4b401f3e8b4e2
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-15 21:32:48 +0000

    generators in ES6

--- js/ES6/generators.js
+++ js/ES6/generators.js
@@ -5,7 +5,7 @@ const colors = ['red', 'blue', 'pink', 'green'];
 for (let color of colors) {
 	console.log(color);
 }
-console.log('==================');
+console.log('====================================');
 
 function* numbers() { //* - is a sign for generators
 	yield 'cash';
@@ -20,3 +20,21 @@ const gen = numbers();
 console.log(gen.next());
 console.log(gen.next());
 console.log(gen.next('0 cash'));
+
+console.log('====================================');
+
+function* brands() {
+	const dg = yield 'DG';
+	const lv = yield 'LV';
+	const prada = yield 'Prada';
+	const hermes = yield 'Hermes';
+
+	return [dg, lv, prada, hermes];
+}
+
+const banchOfBrands = brands();
+console.log(banchOfBrands.next('dg t-shirt'));
+console.log(banchOfBrands.next('lv t-shirt'));
+console.log(banchOfBrands.next('prada pants'));
+console.log(banchOfBrands.next('Hermes bag'));
+console.log(banchOfBrands.next('Shopping list has been done'));
