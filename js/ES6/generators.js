bb50b0a20081d954ddd5d7b907dd052477971894
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-15 21:02:40 +0000

    generators in ES6

--- js/ES6/generators.js
+++ js/ES6/generators.js
@@ -5,3 +5,9 @@ const colors = ['red', 'blue', 'pink', 'green'];
 for (let color of colors) {
 	console.log(color);
 }
+console.log('==================');
+
+function* numbers() { //* - is a sign for generators
+
+}
+console.log(numbers());
