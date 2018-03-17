f3f8e7b4a16a690a709d8f12b4493ff2acf386ff
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-17 12:51:45 +0000

    fetch in ES6

--- js/ES6/promisses_fetch.js
+++ js/ES6/promisses_fetch.js
@@ -1,14 +1,8 @@
 const url = 'https://jsonplaceholder.typicode.com/posts/';
-
-const responsePromise = new Promise((resolve, reject) => {
-	let response = fetch(url);
-	resolve(response);
-});
-
 //fetch is a pre-build promise which return just a response without data
 fetch(url)
 	.then(response => console.log('Success: ', response))
-	.catch(response => console.log('Failed: ',response));
+	.catch(error => console.log('Failed: ', error));
 
 //to get data from our response we need to write next code
 fetch(url)
